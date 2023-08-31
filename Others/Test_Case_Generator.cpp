6a54a367f2c485adb13246509e7b45fc196c0e41
Abid Hussen <124859338+AbiDHussen@users.noreply.github.com>
2023-08-31 13:28:56 +0600

    Add files via upload

---
+++ Others/Test_Case_Generator.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); /// mt19937_64 (long long)
+auto my_rand(long long l,long long r)   // random value generator [l, r]
+{
+    return uniform_int_distribution<long long>(l,r)(rng);
+}
+
+int lowerVal, upperVal;
+int randomize() // function to generate random numbers in range [lowerval - upperVal] 
+{
+    return my_rand(lowerVal, upperVal);
+}
+
+int main()
+{
+    srand(time(NULL)); // for different values each time we run the code
+
+    int n = my_rand(1, 5);
+    // cin>>n;
+
+    vector<int> vec(n);
+
+    lowerVal = 1, upperVal = 100;
+    // generate(vec.begin(), vec.end(), randomize);
+    for(int i=0;i<n;i++)
+    {
+        vec[i]=my_rand(lowerVal,upperVal);
+    }
+    // displaying the content of vector
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+
+    return 0;
+}
