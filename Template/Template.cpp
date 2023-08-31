6e41d61bc1ebb1f90a80b197a0cb14fffbcea6b2
Abid Hussen <124859338+AbiDHussen@users.noreply.github.com>
2023-08-31 12:57:41 +0600

    Add files via upload

---
+++ Template/Template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+#define mod 100000007
+// #define int long long
+#define all(x)      x.begin(),x.end()
+#define print(x) cout << #x << " : " << x << endl
+#define error1(x)   cerr << #x << " = " << (x) <<endl
+#define coutall(v)  for(auto &it: v) cout<<it<<' '; cout<<endl
+#define Abid_52   ios::sync_with_stdio(false);cin.tie(0),cin.tie(0)
+#define error2(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n"
+const int N = 1e5 + 10;
+void solve()
+{
+    int n,m;
+    cin >> n;
+    vector<int> v(n+5);
+    for (int i = 0; i < n; i++)cin >> v[i];
+}
+int32_t main()
+{
+    Abid_52;
+    int t = 1;
+    // cin >> t;
+    for (int tc = 1; tc <= t; ++tc)
+    {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+}
