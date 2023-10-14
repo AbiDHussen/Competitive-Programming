ecfef3abfc3d9d59dbfa44f2198f2f393bde95e2
Abid Hussen <124859338+AbiDHussen@users.noreply.github.com>
2023-10-15 00:50:56 +0600

    Update Template.cpp

--- Template/Template.cpp
+++ Template/Template.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define endl '\n'
 using namespace std;
+using namespace __gnu_pbds;
 #define ll long long
 #define mod 100000007
 // #define int long long
@@ -19,6 +22,8 @@ using namespace std;
 #define UNIQUE(x)   sort(all(x)), x.erase(unique(all(x)), x.end()), x.shrink_to_fit()
 template <typename T, typename U> T ceil(T x, U y) {return (x > 0 ? (x + y - 1) / y : x / y);}
 template <typename T, typename U> T floor(T x, U y) {return (x > 0 ? x / y : (x - y + 1) / y);}
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R> using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 const int N = 1e5 + 10;
 void solve()
 {
