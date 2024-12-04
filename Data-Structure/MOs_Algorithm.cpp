b37b428343d2ec14c696a228a6dce71a58e67def
Abid Hussen <124859338+AbiDHussen@users.noreply.github.com>
2024-12-05 03:23:17 +0600

    Update MOs_Algorithm.cpp

--- Data-Structure/MOs_Algorithm.cpp
+++ Data-Structure/MOs_Algorithm.cpp
@@ -1,105 +1,69 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>    //=> O((N+Q) * sqrt(N))
+#define endl "\n"
+#define ll long long
 using namespace std;
 
-// Speed
-#define Code ios_base::sync_with_stdio(false);
-#define By cin.tie(NULL);
-#define Abid cout.tie(NULL);
-
-using vi = vector<int>;
-using vs = vector<string>;
-using ll = long long;
-
-#define int long long
-#define mod 100000007
-#define endl '\n'
-#define coutall(v)         \
-    for (auto &it : v)     \
-        cout << it << ' '; \
-    cout << endl;
-const int N = 1e6 + 10;
+const int N = 1e6 + 7;
 int rootN;
-struct query
-{
-    int idx, l, r;
-} Q[N];
-bool compare(query q1, query q2)
-{
-    if (q1.l / rootN == q2.l / rootN)
-    {
-        return q1.r > q2.r;
-    }
-    return q1.l < q2.l;
+
+struct Q {
+    int l, r, idx;
+};
+Q q[N];
+
+bool comp(Q q1, Q q2) {
+    if (q1.l / rootN == q2.l / rootN) return q1.r > q2.r;
+    return q1.l / rootN < q2.l / rootN;
 }
-void solve()
-{
-    int n, q;
-    cin >> n >> q;
-    vector<int> v(n);
-    for (ll i = 0; i < n; i++)
-    {
-        cin >> v[i];
+
+// bool comp(Q p, Q q) { // faster 
+//     if (p.l / rootN != q.l / rootN) return (p.l == q.l ? p.r <= q.r : p.l < q.l); // return p < q;
+//     return (p.l / rootN & 1) ? (p.r < q.r) : (p.r > q.r);
+// }
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int n; cin >> n;
+    int a[n];
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    int query; cin >> query;
+    rootN = sqrtl(n) + 1;
+    for (int i = 0; i < query; ++i) {
+        int l, r; cin >> l >> r;
+        q[i].l = l;
+        q[i].r = r;
+        q[i].idx = i;
     }
-    rootN = sqrtl(n);
-    for (int i = 0; i < q; i++)
-    {
-        int l, r;
-        cin >> l >> r;
-        Q[i].l = l;
-        Q[i].r = r;
-        Q[i].idx = i;
-    }
-    sort(Q, Q + q, compare);
-    // for (int i = 0; i < q; i++)
-    // {
-    //     cout << Q[i].idx << " " << Q[i].l << " " << Q[i].r << endl;
-    // }
-    vector<int> ans(q);
+
+    sort(q, q + query, comp);
     int curr_l = 0, curr_r = -1, l, r;
-    int curr_ans = 0;
-    for (int i = 0; i < q; i++)
-    {
-        l = Q[i].l;
-        r = Q[i].r;
-        l--;
-        r--;
-        while (curr_r < r)
-        {
-            curr_r++;
-            curr_ans += v[curr_r];
+    ll curr_ans = 0;
+    vector<ll> ans(query);
+    for (int i = 0; i < query; i++) {
+        l = q[i].l, r = q[i].r;
+        --l, --r;
+        while (curr_r < r) {
+            ++curr_r;
+            curr_ans += a[curr_r];
         }
-        while (curr_l > l)
-        {
-            curr_l--;
-            curr_ans += v[curr_l];
+        while (curr_l > l) {
+            --curr_l;
+            curr_ans += a[curr_l];
         }
-        while (curr_r > r)
-        {
-            curr_ans -= v[curr_r];
-            curr_r--;
+        
+        while (curr_l < l) {
+            curr_ans -= a[curr_l];
+            ++curr_l;
         }
-        while (curr_l < l)
-        {
-            curr_ans -= v[curr_l];
-            curr_l++;
+        while (curr_r > r) {
+            curr_ans -= a[curr_r];
+            --curr_r;
         }
-        ans[Q[i].idx] = curr_ans;
+        ans[q[i].idx] = curr_ans;
     }
-    // coutall(ans);
-    for (int i = 0; i < ans.size(); i++)
-    {
+    
+    for (int i = 0; i < query; i++) {
         cout << ans[i] << endl;
     }
-}
-
-int32_t main()
-{
-    // Code By Abid
-    int t = 1;
-    // cin >> t;
-    for (int tc = 1; tc <= t; ++tc)
-    {
-        // cout << "Case " << tc << ":";
-        solve();
-    }
+    return 0;
 }
